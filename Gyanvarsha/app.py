ab2696c8b2bbeb30c99873643450ac7162d8840a
Prabhat Tripathi <65029713+darkest-opson@users.noreply.github.com>
2024-03-07 23:16:15 +0530

    Add files via upload

---
+++ Gyanvarsha/app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+# The URL to redirect to
+# redirect_url = "https://www.google.com"
+
+# HTML code for the redirection
+redirect_html = f"""
+    <html>
+        <head>
+            <meta http-equiv="refresh" content="0;URL='{redirect_url}'" />
+        </head>
+        <body>
+            <a href="{redirect_url}">Gyanvarsha</a>...
+        </body>
+    </html>
+"""
+
+# Display the HTML code
+st.markdown(redirect_html, unsafe_allow_html=True)
