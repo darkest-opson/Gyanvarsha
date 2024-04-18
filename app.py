54f28f7c016e94baf86abd8f2331dc5990112c75
Prabhat Tripathi <65029713+darkest-opson@users.noreply.github.com>
2024-04-18 23:34:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask,render_template,redirect,request,send_file
+import csv
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route('/submit', methods=['POST'])
+def getdata():
+    name = request.form['name']
+    email = request.form['email']
+    contact_number = request.form['contact_number']
+    message = request.form['message']
+        # Write form data to a CSV file
+    with open("forms.csv", mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([name,email,contact_number,message])
+    return "<h1>Thank You \n Data submitten successfully</h1>"
+        
+    #     # Redirect the user to the homepage after form submission
+    # return "Thanks to you"
+
+@app.route('/lectures') 
+def lectures():
+    return render_template('lectures.html')
+
+@app.route('/responses')
+def download_csv():
+    # Specify the path to your CSV file
+    csv_path = 'forms.csv'
+    
+    # Send the file as an attachment
+    return send_file(csv_path, as_attachment=True)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
